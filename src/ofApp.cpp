722124cd0f41e3594b99d05903f2f943f56e839e
THOMAS NIKOLOPOULOS <tommot38@gmail.com>
2024-12-18 15:56:46 +0200

    particle class and stuff

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,7 +2,11 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    ofSetBackgroundColor(0,0,0);
+    Create_particles();
+    
+    
+    
 }
 
 //--------------------------------------------------------------
@@ -12,7 +16,10 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+for (int i = 0; i < particles.size(); i++)
+    {
+        particles[i].draw();
+    }
 }
 
 //--------------------------------------------------------------
@@ -68,4 +75,28 @@ void ofApp::gotMessage(ofMessage msg){
 //--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
 
+}
+
+Particle::Particle(float x, float y, ofColor colour) {
+    position = glm::vec2(x,y);          // Initialize position with input coordinates
+    velocity = glm::vec2(0.0f,0.0f);    // Start with zero velocity
+    color = colour;                      // Assign the particle type
+}
+
+void Particle::draw() {
+    ofSetColor(color);
+    ofDrawCircle(position,1);
+}
+
+// Creates a specifc number of every particle type and adds them to the vector of particles
+// Every particle is initialized with random positions
+void ofApp::Create_particles(){
+    for (int i = 0; i < number_of_particles; i++)
+    {
+        for (int i=0; i<color_types.size(); i++)
+        {
+            Particle newParticle(ofRandom(MAP_WIDTH),ofRandom(MAP_HEIGHT),color_types[i]);
+            particles.push_back(newParticle);
+        }
+    }       
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,6 +2,20 @@
 
 #include "ofMain.h"
 
+class Particle {
+	public:
+	//glm vector -> better performance for graphics
+	glm::vec2 position; 	// 2D vector representing the position (x, y)
+	glm::vec2 velocity;		// 2D vector representing the velocity (vx, vy)
+	ofColor color;			// Type of the particle (for interaction rules)
+
+	Particle(float x, float y, ofColor color);
+	// ~Particle();
+
+	void update();
+	void draw();
+};
+
 class ofApp : public ofBaseApp{
 
 	public:
@@ -20,5 +34,30 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+
+		// Constant Variables
+		const short MAP_BORDER = 5;
+		const short MAP_WIDTH = 800 + MAP_BORDER;
+		const short MAP_HEIGHT = 800 + MAP_BORDER;
+		const short ATOM_WIDTH = 1;
+		const float MAX_FORCE = 25;
+		const short WALL_REPEL_BOUND = MAP_BORDER+4;  // the wall starts repelling particles if they're closer than WALL_REPEL_BOUND pixels
+		const float WALL_REPEL_FORCE = 0.1;
+		const int NUM_TYPES = 3;    // Number of different particle types
+		#define RED 0
+		#define GREEN 1
+		#define YELLOW 2
+		short FORCE_RANGE = 200;
+		short number_of_particles = 450;    // per type (color)
+		short total_particles = number_of_particles*NUM_TYPES;
+
+		std::array<ofColor,3> color_types = {
+			ofColor::red,	// 0
+			ofColor::green,	// 1
+			ofColor::yellow	// 2
+		};
+
+		vector<Particle> particles;		// vector containing all particles;
 		
+		void Create_particles();
 };
